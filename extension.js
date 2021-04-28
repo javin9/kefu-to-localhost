5e4342a60a3fa340d9162c9f74006f1301da160c
liujianwei1 <liujianwei1@tal.com>
2021-04-28 14:19:39 +0800

    feat: 完成替换

--- extension.js
+++ extension.js
@@ -9,6 +9,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode')
+const fs = require('fs-extra')
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -25,9 +26,11 @@ function activate (context) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
+  let disposable = vscode.commands.registerCommand('extension.helloWorld', function (args) {
     // The code you place here will be executed every time your command is executed
-
+    const data = fs.readFileSync(args.path, { encoding: 'utf8' })
+    const content = data.replace(/(kefu.xesv5.com)|(\.kefu.xesv5.com)/ig, 'localhost')
+    fs.writeFileSync(args.path, content)
     // Display a message box to the user
     vscode.window.showInformationMessage('Hello World from extension!')
   })
